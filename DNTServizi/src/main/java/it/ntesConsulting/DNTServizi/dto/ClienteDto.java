ed9d8629404af4f0a0e4f9ea552ab22d882ef4bc
Etienne <tadonntes@gmail.com>
2025-01-09 16:38:01 +0100

    remove dnt

--- DNTServizi/src/main/java/it/ntesConsulting/DNTServizi/dto/ClienteDto.java
+++
@@ -1,7 +0,0 @@
-package it.ntesConsulting.DNTServizi.dto;
-
-
-public class ClienteDto extends DipendenteDto {
-    
-}
-
--- DNTServizi/src/main/java/it/ntesConsulting/DNTServizi/enums/StatoLavoro.java
+++
@@ -1,5 +0,0 @@
-package it.ntesConsulting.DNTServizi.enums;
-
-public enum StatoLavoro {
-CREATO,ASSEGNATO,INNIZIATO,META,FATTO;
-}
