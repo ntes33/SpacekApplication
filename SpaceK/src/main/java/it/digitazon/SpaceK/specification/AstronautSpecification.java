f71f42161d6f07e767fe61d1c10cb82782f4e1d8
Etienne <tadonntes@gmail.com>
2025-01-09 14:35:53 +0100

    update

--- SpaceK/src/main/java/it/digitazon/SpaceK/specification/AstronautSpecification.java
+++ SpaceK/src/main/java/it/digitazon/SpaceK/specification/AstronautSpecification.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.domain.Specification;
 import it.digitazon.SpaceK.entity.Astronaut;
 
 public class AstronautSpecification {
-	//cerca in funzione della disponibilita dell'astronauta
+	//cerca in funzione della disponibilita dell'astronauta (filtro)
 	 public static Specification<Astronaut> isAvailable(Boolean available) {
 	        return (root, query, criteriaBuilder) -> 
 	            available != null ? criteriaBuilder.equal(root.get("isAvailable"), available) : null;
